c5cb2cb4019a87fe8e87b4c54a43be1f293da3cc
Jonatas Gama Souza <gamasouza.jonatas@gmail.com>
2024-01-03 23:44:19 -0300

    criando frontend de logout

--- resources/views/livewire/auth/logout.blade.php
+++ resources/views/livewire/auth/logout.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logout" wire:click="logout"/>
 </div>
